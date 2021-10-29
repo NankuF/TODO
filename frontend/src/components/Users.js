bc5e4096e3db4d2a92301b7efd372cbb3dc036ef
nankuF <fireloki87@gmail.com>
2021-10-29 15:18:17 +0300

    create Users components install corsheaders

---
+++ frontend/src/components/Users.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const UserItem = ({user}) => {
+    return (
+        <tr>
+            <td>
+                {user.first_name}
+            </td>
+            <td>
+                {user.last_name}
+            </td>
+            <td>
+                {user.username}
+            </td>
+        </tr>
+    )
+}
+
+const UserList = ({users}) => {
+    return (
+ <table>
+           <th>
+               Firstname
+           </th>
+           <th>
+               Lastname
+           </th>
+           <th>
+               Username
+           </th>
+           {users.map((user) => <UserItem user={user} />)}
+       </table>
+
+    )
+}
+
+export default UserList;
