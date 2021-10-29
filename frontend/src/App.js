932d5cfab94cfae95b8129b786d3a3e91edb5d49
nankuF <fireloki87@gmail.com>
2021-10-29 15:27:57 +0300

    get data from backend add node_modules to git

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,35 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import UserList from "./components/Users";
+import axios from "axios";
 
-class App extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      'users': []
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'users': []
+        }
     }
-  }
 
-  render(){
-    return (
-        <div>
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/users').then(
+            response => {
+                const users = response.data
+                this.setState(
+                    {'users': users}
+                )
+            }
+        ).catch(error => console.log(error))
+    }
 
-        </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <UserList users={this.state.users}/>
+            </div>
+        )
+    }
 }
 
 export default App;
