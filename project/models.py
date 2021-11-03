3e9bcb035610bd4450209605e6a59238f6fbbdb9
nankuF <fireloki87@gmail.com>
2021-11-03 13:22:45 +0300

    add models Project, ToDo.

--- project/models.py
+++ project/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from users.models import CustomUser
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    repository = models.URLField(blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class ToDo(models.Model):
+    """Заметка"""
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    description = models.DateTimeField()
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.description}'
+
--- users/models.py
+++ users/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from project.models import Project
+
 
 class CustomUser(AbstractUser):
+    project = models.ManyToManyField(Project)
     email = models.EmailField(unique=True)
