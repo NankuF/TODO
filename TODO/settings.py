249bd799570ceef38d70b96ea81a4d187c84cc15
nankuF <fireloki87@gmail.com>
2021-11-13 18:04:00 +0300

    Права юзерам выдаются через Джанго, анонимы имеют право только на GET; добавлено 3 группы юзеров; добавлена базовая авторизация(логин пароль); добавлена авторизация по токену; добавлена авторизация через сессию для внутренних нужд.

--- TODO/settings.py
+++ TODO/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import rest_framework.authentication
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
     'users.apps.UsersConfig',
     'project.apps.ProjectConfig',
@@ -134,5 +137,10 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # REST_FRAMEWORK
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'],
+    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication',
+                                       'rest_framework.authentication.SessionAuthentication',
+                                       'rest_framework.authentication.BasicAuthentication']
+
 }
